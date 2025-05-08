fd067eac3ad80f0c47359a9ac161a2b100cf80ad
gpfe <arcd009999@gmail.com>
2025-05-08 11:24:57 +0200

    create a polymorphic association for message model
    enables messages to be sent to users and to chat simultaneousely

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :messages, as: :receivable
+  has_many :sent_messages, foreign_key: "sender_id", class_name: "Message"
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
